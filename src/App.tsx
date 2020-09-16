4e395748e904077acaafaf25790e89fda85059a5
DariusPirvulescu <organicdarius@gmail.com>
2020-09-16 17:23:02 +0300

    Improve setTodos

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import React, { useState } from 'react';
 import ToDoList from "./components/ToDoList";
 import AddToDo from "./components/AddToDo";
-// import { stringify } from 'querystring';
 import { ToDo } from "./todo.model";
 
 const App: React.FC = () => {
-  // const todo = [{id: "1", task: "Learn React-TS"}]
   const [ todos, setTodos ] = useState<ToDo[]>([{ id: "1", task: "Learn React with TS" }])
   
   
   const getInputText = (text: string) => {
     console.log(text)
-    setTodos([...todos, 
+    setTodos(prevTodos => [
+      ...prevTodos, 
       {id: Math.random().toString(), task: text}
     ])
   }
