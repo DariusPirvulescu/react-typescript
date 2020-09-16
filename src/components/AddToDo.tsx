bd9da18a3877db2bdd5c7e1cfeb50ba850f84ade
DariusPirvulescu <organicdarius@gmail.com>
2020-09-16 21:13:36 +0300

    Add basic validation, Empty input after submit

--- src/components/AddToDo.tsx
+++ src/components/AddToDo.tsx
@@ -6,14 +6,20 @@ interface AddToDoProps {
   submit: (text: string) => void
 }
 
+
 const AddToDo: React.FC<AddToDoProps> = props => {
   const inputRef = useRef<HTMLInputElement>(null)
 
   const handleSubmit = (evt: React.FormEvent) => {
     evt.preventDefault();
-
+    
     const inputTask = inputRef.current!.value
-    props.submit(inputTask)
+    if (inputTask.length > 0) {
+      props.submit(inputTask)
+    } else {
+      alert("Task input cannot be empty")
+    }
+    inputRef.current!.value = ""
     // return;
   }
 
